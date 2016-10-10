c10502ed55b211a81bc3971d3401386229220569
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-10-10 22:32:25 +0530

    Add files via upload

---
+++ Memory-Fit-Algos/first_fit.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> a[100];
+char s[100];
+main()
+{
+	printf("\nEnter the memory list\n0 for free space\n1 for occupied\n\n");
+	scanf("%s",s);
+	int l=strlen(s);
+	int ind=0;
+	for(int i=0;i<l;i++)
+	{
+		if(s[i]=='0')
+		{
+			a[ind].first=i;
+
+			while(s[i]=='0'&&i<l)                                     //   
+			{                                                         //Keeps record of free areas
+				i++;                                              //
+				a[ind].second=i-1;                  
+
+			}
+			ind++;
+		}
+	}
+	printf("\nNo. of free areas with their size are : \n");
+	printf("%d\n",ind);                                                     //No. of free areas
+	for(int i=0;i<ind;i++) printf("%d ",a[i].second-a[i].first+1);        //Start and end of free areas 
+	int n;
+	printf("\nEnter no. of processes\n");
+	scanf("%d",&n);
+	int ct=0;
+	while(n--)
+	{
+		ct++;
+		int flg=0;
+		printf("Enter memory alloted to process %d\n",ct);
+		int mem;
+		scanf("%d",&mem);
+		for(int i=0;i<ind;i++)
+		{
+			if(a[i].second-a[i].first+1>=mem)
+			{
+				flg=1;
+				for(int j=a[i].first;j<a[i].first+mem;j++) s[j]='1';
+				a[i].first+=mem;
+				break;
+			}
+		}
+		if(flg==0) printf("Not Alloted");
+	}
+	printf("\nResultant memory list is :\n");
+	printf("%s\n",s);
+	printf("Resultant free area sizes are :\n");
+	for(int i=0;i<ind;i++) printf("%d ",a[i].second-a[i].first+1);  
+	printf("\n\n");
+}
