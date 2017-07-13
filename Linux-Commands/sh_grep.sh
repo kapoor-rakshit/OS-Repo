3d06fbf11746a490b23a574a2dd54aa6d64572cd
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-07-13 10:35:26 +0530

    grep

--- Linux-Commands/sh_grep.sh
+++ Linux-Commands/sh_grep.sh
@@ -10,9 +10,15 @@
 # -c : Prints only the count of matching lines.
 # -i : Matches either upper or lowercase.
 # -w : matches by treating the "arg" as a WORD to be searched and not as a SUBSTRING of any word
+# ^word : matches lines in file that STARTS with "word"
+# word$ : matches lines in file that ENDS with "word"
+# --color : highlights the search
+
+grep “fast.*host” index.html         # starts with “fast” and ends with “host” with anything in-between
+
+grep “linux” file*.*                #  all files with file1.txt, file2.txt and along with different extensions too like file1.html, file2.php and so on.
 
 ls -l | grep -i -c "Jul"
-
 
 grep -w "the" file.txt              #searches the word "the" and echo that line, not work for "there" as it has "the" as SUBSTRING not a word
 
