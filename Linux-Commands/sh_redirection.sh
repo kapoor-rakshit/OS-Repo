6f8595d0cadf944083e9569e1d8a92dc3b9d4974
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-03-31 20:20:09 +0530

    got about here document

--- Linux-Commands/sh_redirection.sh
+++ Linux-Commands/sh_redirection.sh
@@ -9,6 +9,9 @@
 echo "Overwritten on `date`" > content.txt   # > overwrites the existing content of file
 echo "Appended on `date`" >> content.txt     # >> appends to file
 
+#Read from STDIN and write to file
+cat > filename.txt                           # read from stdin and write to file
+                                             # press ctrl + D, to terminate further input
 #Read from a file
 wc < content.txt                             #Uses the contents of content.txt to act on wc command
 wc content.txt                               #shows filename with results
@@ -18,20 +21,21 @@ wc content.txt                               #shows filename with results
 # HERE document is used to redirect input to an interactive shell script or program without providing user input
 #Following codes can be written on prompt itself :
 
-#1) wc -l << finish                        #"finish" is a delimiter which can be single word without any whitespace
-  # Hello working at WIPRO
-  # What abt you ?
-  # finish                                 #OUTPUT : 2
+wc -l <<finish                        # "finish" is a delimiter which can be single word without any whitespace 
+                                      # and without any space bw "<<" and "finish" (delim)
+Hello working at WIPRO
+What abt you ?
+finish                                 # Terminate prog , OUTPUT : 2   no. of lines, as of command wc -l
 
 
 # A session can be run with vi editor and save input in the file
-#2) filename=test.txt
-  # vi $filename << finish
-#   i
- #  written using HERE doc
-#   ^[
-#   ZZ
-#   finish
+filename=test.txt
+vi $filename <<END
+i
+written using HERE doc
+^[
+ZZ
+finish
 
 
 # REDIRECTION : To execcute the command but donot show the OUTPUT on screen (STDOUT) or file
@@ -40,6 +44,7 @@ echo "`uptime`" > /dev/null            #/dev/null is a file that discards all in
 
 echo "`upt`" > /dev/null               #STDERR will be redirected but not STDOUT as command not found
 
-echo "hello printing STDOUT and STDERR" &> output_and_err.txt     # & : redirect both STDOUT and STDERR
+echo "hello printing STDOUT and STDERR" &> output_and_err.txt     # & : redirect both STDOUT and STDERR to same file
 
+echo "testing" 1> output.txt 2> err.txt                           # STDOUT to output.txt and STDERR to err.txt 
                                                                                                                
