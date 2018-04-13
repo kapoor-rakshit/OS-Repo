16864496536ff3a0643836896bdb46f0a481ad57
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-04-13 18:01:24 +0530

    metachars added

--- Linux-Commands/sh_grep.sh
+++ Linux-Commands/sh_grep.sh
@@ -3,18 +3,32 @@
 
 #a pipe ie a vertical bar (|) on the command line between two commands
 #The name "grep" comes from the ed (a Unix line editor) command g/re/p which means “globally search for a regular expression and print all lines containing it”.
-#Options
+
+# Options
 # -v : Prints all lines that do not match pattern.
 # -n : Prints the matched line and its line number.
 # -l : Prints only the names of files with matching lines.
 # -c : Prints only the count of matching lines.
 # -i : Matches either upper or lowercase.
 # -w : matches by treating the "arg" as a WORD to be searched and not as a SUBSTRING of any word
-# ^word : matches lines in file that STARTS with "word"
-# word$ : matches lines in file that ENDS with "word"
+
+# Metacharacters
+# f...l : (dot) represents a character
+# f*fox : (asterisk) one or more
+# ^word : (carat) matches lines in file that STARTS with "word"
+# word$ : (dollar) matches lines in file that ENDS with "word"
+# f\\.t : (\\) cancel meaning of metacharacter, here "." acts as a symbol not as a metacharacter, so search has f.t only, not fit or fat
+# []    : (range operator)   eg: a[df]b , a followed by one char either "d" or "f" followed by b
+#                                a[^df]b , a followed by one char neither "d" nor "f" followed by b
+ #  [:lower:] or [a-z]                       char with lowercase
+ #  [:upper:] or [A-Z]
+ #  [:alpha:] or [a-zA-Z]
+ #  [:digit:] or [0-9]
+ #  [:alnum:] or [a-zA-Z0-9]
+ #  [:blank:]
 # --color : highlights the search
 
-grep “fast.*host” index.html         # starts with “fast” and ends with “host” with anything in-between
+grep “fast*host” index.html         # with “fast”"anything in-between"“host” 
 
 grep “linux” file*.*                #  all files with file1.txt, file2.txt and along with different extensions too like file1.html, file2.php and so on.
 
