84d285e0570412143ea3733c6aad85f75c0e090a
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-09-05 12:26:22 +0530

    Update sh_init.sh

--- Linux-Commands/sh_init.sh
+++ Linux-Commands/sh_init.sh
@@ -4,20 +4,19 @@
 
 
 
-
 # to run another file from within bash file...use following bash script
 
  #!/bin/bash
  
- SCRIPT_PATH="myPythonscript.py" 
+ SCRIPT_PATH="myPythonscript.py"            # vars
  PYTHON_PATH="/usr/bin/python"
  
- # call script via the interpreter     
- $PYTHON $SCRIPT_PATH
+ # call script from within another script using vars
+ $PYTHON_PATH $SCRIPT_PATH
  
  # or
  
- # call script via the interpreter
+ # call script using command (python3) and not var
  python3 $SCRIPT_PATH
  
  
