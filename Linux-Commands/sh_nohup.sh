991eb5c1d3e63492878157af19a5fa7fb5e8473c
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2019-03-14 11:27:32 +0530

    Create sh_nohup.sh

---
+++ Linux-Commands/sh_nohup.sh
@@ -0,0 +1,11 @@
+
+# REFERENCE : https://www.computerhope.com/unix/unohup.htm
+
+# nohup
+# When using the command shell, prefixing a command with nohup prevents the command from being aborted automatically when you log out or exit the shell.
+# The name nohup stands for "no hangup." 
+# The hangup (HUP) signal, which is normally sent to a process to inform it that the user has logged off (or "hung up"), 
+# is intercepted by nohup, allowing the process to continue running.
+
+nohup COMMAND
+
