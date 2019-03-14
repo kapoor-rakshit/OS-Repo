0c9782abadbeb02ddf2fb0d3225cd51ca7947bb4
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2019-03-14 11:21:59 +0530

    Create sh_curl.sh

---
+++ Linux-Commands/sh_curl.sh
@@ -0,0 +1,10 @@
+
+# REFERENCE : https://www.computerhope.com/unix/curl.htm
+
+# cURL
+# curl is a tool to transfer data from or to a server, using one of the supported protocols (HTTP, HTTPS, FTP, FTPS, SCP, SFTP, TFTP, DICT, TELNET, LDAP or FILE).
+# The command is designed to work without user interaction.
+# It offers proxy support, user authentication, FTP uploading, HTTP posting, SSL connections, cookies, file transfer resume, Metalink etc.
+
+curl <URL>
+
